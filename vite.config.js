a88afcded001eb1cee32e08dda39e05bd058ae45
woowooyong <woowooyong@gmail.com>
2023-05-11 00:33:14 +0800

    update: add deploy base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Vue-week4-TodoList-with-Composition-API',
   plugins: [vue()],
   resolve: {
     alias: {
